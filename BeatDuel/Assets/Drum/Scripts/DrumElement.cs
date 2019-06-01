72dcd988a7eeabf5499f620d40c77903aa4d1ec7
Vinicius Uzeda <vruzeda@gmail.com>
2019-06-01 00:50:55 -0300

    Make drum kit even more Unity-y

--- BeatDuel/Assets/Drum/Scripts/DrumElement.cs
+++ BeatDuel/Assets/Drum/Scripts/DrumElement.cs
@@ -4,14 +4,10 @@ using UnityEngine.Events;
 public class DrumElement : MonoBehaviour
 {
     [SerializeField]
-    private AudioSource audioSource;
-
-    [SerializeField]
     private UnityEvent OnDrumElementHitEvent;
 
     public void DrumElementHit()
     {
-        audioSource?.Play();
         OnDrumElementHitEvent?.Invoke();
     }
 }
