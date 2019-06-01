6435d2feb429a1f0fc80bc803de26ebd8cad570b
Vinicius Uzeda <vruzeda@gmail.com>
2019-06-01 00:10:49 -0300

    Initial drum implementation

---
+++ BeatDuel/Assets/Drum/Scripts/DrumElement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DrumElement : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private UnityEvent OnDrumElementHitEvent;
+
+    public void DrumElementHit()
+    {
+        audioSource?.Play();
+        OnDrumElementHitEvent?.Invoke();
+    }
+}
---
+++ BeatDuel/Assets/Drum/Scripts/DrumKit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DrumKit : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent<int> OnDrumKitHitEvent;
+
+    public void DrumElementHit(int index)
+    {
+        OnDrumKitHitEvent?.Invoke(index);
+    }
+}
