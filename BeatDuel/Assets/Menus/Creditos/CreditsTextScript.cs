6c5d5b7d7f7f14d47c4612cf0540f47b7fe61ca8
Piramba <avpaganini@gmail.com>
2019-06-01 17:17:03 -0300

    Credits

---
+++ BeatDuel/Assets/Menus/Creditos/CreditsTextScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class CreditsTextScript : MonoBehaviour
+{
+    public int position;
+    public bool isLast = false;
+
+    RectTransform rTransform;
+
+    Vector3 startPosition;
+    float velocity = 0.004f;
+    Vector3 endPosition;
+    bool active = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rTransform = GetComponent<RectTransform>();
+
+        startPosition = rTransform.position;
+        endPosition = startPosition * -1;
+        if (isLast)
+        {
+            endPosition = Vector3.zero;
+        }
+        Invoke("Activate", position);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (active)
+        {
+            rTransform.position = Vector3.Lerp(
+                rTransform.position,
+                endPosition,
+                velocity
+            );
+
+            if (isLast)
+            {
+                rTransform.Rotate(Vector3.forward, 1f);
+            }
+        }
+    }
+
+    void Activate()
+    {
+        active = true;
+    }
+}
---
+++ BeatDuel/Assets/Menus/Creditos/ExitCredits.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitCredits : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
