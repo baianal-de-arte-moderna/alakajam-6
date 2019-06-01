f297ec2c8e8ff02ed88763d6c3effe9841e37498
vgardelli <vgardelli@daitangroup.com>
2019-06-01 13:13:22 -0300

    Improvements to GridView.
      - Change GridView to use Lerp for spreading plays in grid.
      - Improve GridView to spread multiple plays on same subdivision as well.

--- BeatDuel/Assets/Scripts/GridView.cs
+++ BeatDuel/Assets/Scripts/GridView.cs
@@ -17,6 +17,7 @@ public class GridView : MonoBehaviour
     private Dictionary<int, List<GameObject>> currentPlays;
     private float SCREENWIDTH;
     private float SCREENWIDTH_2;
+    private float GRIDHEIGHT_2;
 
     public void RegisterPlay(int subdivision, int playerIndex)
     {
@@ -27,12 +28,17 @@ public class GridView : MonoBehaviour
     private void ArrangePlaysInSubdivision(int subdivision, List<GameObject> plays)
     {
         //float subdivisionLineOffset = subdivision * gridLineWidth;
-        float subdivisionSlotOffset = subdivision * (SCREENWIDTH / (subdivisions));
+        //float subdivisionSlotOffset = subdivision * (SCREENWIDTH / (subdivisions));
         //float playPosition = gridMarginOffset + subdivisionLineOffset + subdivisionSlotOffset - SCREENWIDTH_2;
-        float playPosition = subdivisionSlotOffset - SCREENWIDTH_2;
-        foreach (GameObject play in plays)
+        //float playPosition = subdivisionSlotOffset - SCREENWIDTH_2;
+        // Distribute all play buckets across the grid.
+        float subdivisionPosition = Mathf.Lerp(-SCREENWIDTH_2, SCREENWIDTH_2, subdivision / (float)subdivisions);
+        for (int currentPlay = 0; currentPlay < plays.Count; currentPlay++)
         {
-            play.transform.position = (float)playPosition * Vector3.right;
+            GameObject play = plays[currentPlay];
+            float playPosition = Mathf.Lerp(-GRIDHEIGHT_2, GRIDHEIGHT_2, currentPlay / (float)plays.Count);
+            play.transform.position = subdivisionPosition * Vector3.right + playPosition * Vector3.up;
+            play.transform.localScale = new Vector3(1.0f, 1.0f / plays.Count, 1.0f);
         }
     }
 
@@ -47,6 +53,7 @@ public class GridView : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GRIDHEIGHT_2 = gridHeight / 2;
         SCREENWIDTH_2 = Camera.main.aspect * Camera.main.orthographicSize;
         SCREENWIDTH = 2 * SCREENWIDTH_2;
         currentPlays = new Dictionary<int, List<GameObject>>();
@@ -60,9 +67,9 @@ public class GridView : MonoBehaviour
     private void TestRegisterPlay()
     {
         int currentPlayer = 0;
-        for (int i = 0; i < subdivisions; i++)
+        for (int i = 0; i < 3 * subdivisions; i++)
         {
-            RegisterPlay(i, currentPlayer);
+            RegisterPlay(i / 3, currentPlayer);
             currentPlayer = (currentPlayer + 1) % 2;
         }
     }
