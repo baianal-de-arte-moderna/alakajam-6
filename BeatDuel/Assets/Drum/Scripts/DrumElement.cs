b89ea683c42351c4179f1a1e3a363ab8b6f7668c
Vinicius Uzeda <vruzeda@gmail.com>
2019-06-01 10:57:58 -0300

    Hit drum element on pointer down

--- BeatDuel/Assets/Drum/Scripts/DrumElement.cs
+++ BeatDuel/Assets/Drum/Scripts/DrumElement.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class DrumElement : MonoBehaviour
+public class DrumElement : MonoBehaviour, IPointerDownHandler
 {
     [SerializeField]
     private UnityEvent OnDrumElementHitEvent;
 
-    public void DrumElementHit()
+    public void OnPointerDown(PointerEventData eventData)
     {
         OnDrumElementHitEvent?.Invoke();
     }
