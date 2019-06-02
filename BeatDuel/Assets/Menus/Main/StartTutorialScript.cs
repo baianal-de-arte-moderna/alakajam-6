b3a24d978070703702831eed63884a37f3b73515
Piramba <avpaganini@gmail.com>
2019-06-02 13:06:07 -0300

    Wiring Tutorial

---
+++ BeatDuel/Assets/Menus/Main/StartTutorialScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartTutorialScript : MonoBehaviour
+{
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+}
