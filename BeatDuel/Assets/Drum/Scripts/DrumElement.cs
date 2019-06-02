675ce88d81181d66b1cc83138937e4f26e3fa984
vgardelli <vgardelli@daitangroup.com>
2019-06-02 11:56:23 -0300

    Add animations to buttons.
      - Add a property to DrumElement to set the color of the button during
        pressed animation.
      - Add pressed animation to DrumElement.
      - Add animation start/end events to DrumElement.

--- BeatDuel/Assets/Drum/Scripts/DrumElement.cs
+++ BeatDuel/Assets/Drum/Scripts/DrumElement.cs
@@ -1,11 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 public class DrumElement : MonoBehaviour, IPointerDownHandler
 {
-    [SerializeField]
-    private UnityEvent OnDrumElementHitEvent;
+    [SerializeField] private Color padColorPressed;
+    [SerializeField] private UnityEvent OnDrumElementHitEvent;
+
+    private Color padColorIdle;
+    public void Start()
+    {
+        padColorIdle = GetComponent<Image>().color;
+    }
+
+    public void OnAnimationStart() {
+        Debug.Log("Animation started");
+        Debug.Log(GetComponent<Image>());
+        GetComponent<Image>().color = padColorPressed;
+    }
+
+    public void OnAnimationEnd() {
+        Debug.Log("Animation ended");
+        GetComponent<Image>().color = padColorIdle;
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
